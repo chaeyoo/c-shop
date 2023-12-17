109238b7f35cfe8c0f8635d98f7d30f1689e6378
chaeyoo <chaeh0124@gmail.com>
2023-12-17 21:55:03 +0900

    [FIX] navbar layout 밖으로

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -6,6 +6,8 @@ import {CiHeart} from "react-icons/ci";
 import {IoPersonOutline} from "react-icons/io5";
 import {GiBackwardTime} from "react-icons/gi";
 import {RiHome3Line} from "react-icons/ri";
+import {IoIosSearch} from "react-icons/io";
+import {SlBasket} from "react-icons/sl";
 
 interface LayoutProps {
     children: ReactNode;
@@ -13,7 +15,30 @@ interface LayoutProps {
 
 export default function Layout({children}: LayoutProps) {
     return (<div className="layout">
-        <Navbar/>
+        <div className="navbar">
+            <div className="navbar__top">
+                <div className="navbar__logo">CSHOP</div>
+                <div className="navbar__icons">
+                    <IoIosSearch size="19"/>
+                    <SlBasket size="19"/>
+                </div>
+            </div>
+
+            <div className="navbar__list">
+                <Link href="/" className="navbar__list--item">
+                    홈
+                </Link>
+                <Link href="/ranking" className="navbar__list--item">
+                    랭킹
+                </Link>
+                <Link href="#" className="navbar__list--item">
+                    매거진
+                </Link>
+                <Link href="#" className="navbar__list--item">
+                    이벤트
+                </Link>
+            </div>
+        </div>
         <div className="main">
             {children}
         </div>
@@ -30,7 +55,7 @@ export default function Layout({children}: LayoutProps) {
             <Link href="#">
                 <IoPersonOutline size={22} color={"white"}/>
             </Link>
-            <Link href="#s">
+            <Link href="#">
                 <GiBackwardTime size={22} color={"white"}/>
             </Link>
         </div>
--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,36 +1,11 @@
 import {useState} from "react";
 import Link from "next/link";
-import {IoIosSearch} from "react-icons/io";
-import {SlBasket} from "react-icons/sl";
 
 
 export default function Navbar() {
     return (<>
 
-        <div className="navbar">
-            <div className="navbar__top">
-                <div className="navbar__logo">CSHOP</div>
-                <div className="navbar__icons">
-                    <IoIosSearch size="19"/>
-                    <SlBasket size="19"/>
-                </div>
-            </div>
 
-            <div className="navbar__list">
-                <Link href="/" className="navbar__list--item">
-                    홈
-                </Link>
-                <Link href="/ranking" className="navbar__list--item">
-                    랭킹
-                </Link>
-                <Link href="#" className="navbar__list--item">
-                    매거진
-                </Link>
-                <Link href="#" className="navbar__list--item">
-                    이벤트
-                </Link>
-            </div>
-        </div>
 
     </>);
 }
