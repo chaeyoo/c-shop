0ccc730287d38c52bc0d5bbb5bbc71f6c091371c
chaeyoo <chaeh0124@gmail.com>
2023-12-26 23:11:39 +0900

    [FEAT] 이모션 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,26 @@
 import Loader from "@/component/loader/Loader";
 import Slideshow from "@/component/slides/Slideshow";
 import {useState} from "react";
+import {css} from "@emotion/react";
+import styled from "@emotion/styled";
 
 export default function Home() {
+    const bold = css`
+    font-weight: bold`;
+    const containerStyles = css`
+        background-color: pink;
+        ${bold};
+        width: 200px;
+        height: 200px;
+    `
+
+    const Button = styled.button`
+        width: 200px;
+        height: 100px;
+        background-color: aquamarine;
+        ${bold};
+    `
+
     const [slideData, setSlideData] = useState<any[]>(
         [
             {"image": "https://picsum.photos/id/237/500/600"},
@@ -14,6 +32,12 @@ export default function Home() {
   return (
           <div>
               <Slideshow slides={slideData} />
+              <div css={containerStyles}>
+                  까꿍
+                    <Button>스타일버튼</Button>
+              </div>
           </div>
+
+
   )
 }
