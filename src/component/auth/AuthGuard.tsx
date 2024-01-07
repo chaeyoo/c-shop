ec10cf6118ab98018d569a7807bc47f1f97e2a56
chaeyoo <chaeh0124@gmail.com>
2024-01-07 22:30:47 +0900

    [FIX] middleware 수정중

--- src/component/auth/AuthGuard.tsx
+++ src/component/auth/AuthGuard.tsx
@@ -3,24 +3,26 @@ import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "@/remote/firebase";
 import { useSetRecoilState } from "recoil";
 import { userAtom } from "@/atoms/user";
-import nookies from "nookies";
-
+// import nookies from "nookies";
+import Cookies from "universal-cookie";
 export default function AuthGuard({ children }: { children: React.ReactNode }) {
 	const [authorize, setAuthorize] = useState<boolean>(false);
 	const setUser = useSetRecoilState(userAtom);
 	onAuthStateChanged(auth, (user) => {
 		console.log(user);
+		const cookies = new Cookies(null, { path: "/" });
 		if (user) {
 			const token = user.uid;
-			nookies.destroy(null, "token");
-			nookies.set(null, "token", token, { path: "/" });
+			cookies.set("token", token);
+			// nookies.destroy(null, "token");
+			// nookies.set(null, "token", token, { path: "/" });
 
 			setUser({
 				uid: user.uid,
 				email: user.email ?? "",
 			});
 		} else {
-			nookies.destroy(null, "token");
+			cookies.remove("token");
 			setUser(null);
 			return;
 		}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,17 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-
+import Cookies from "universal-cookie";
 export async function middleware(request: NextRequest) {
-	let cookie = request.cookies.get("token");
-	console.log(cookie, '>><<<');
-    if (!cookie) {
-        return NextResponse.redirect(new URL("/signin", request.url));
-    }
+	// let cookie = request.cookies.get("token");
+	const cookies = new Cookies(request.cookies, { path: "/" });
+	// console.log(cookies.cookies);
+
+
+	const cookie = cookies.get("token");
+	console.log(cookie, ">><<<");
+	if (!cookie) {
+		return NextResponse.redirect(new URL("/signin", request.url));
+	}
 }
 
 export const config = {
