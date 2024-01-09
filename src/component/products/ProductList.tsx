6b719b4a57ef57a2fa6686fad25fd731abac44d8
chaeyoo <chaeh0124@gmail.com>
2024-01-09 22:21:58 +0900

    [FEAT] 상품 목록페이지 정보 추가

--- src/component/products/ProductList.tsx
+++ src/component/products/ProductList.tsx
@@ -1,82 +1,158 @@
 import ProductListRow from "@/component/products/ProductRow";
 import ProductRow from "@/component/products/ProductRow";
-import {useInfiniteQuery} from "react-query";
-import {getProducts} from "@/remote/products";
-import {flatten} from "lodash";
+import { useInfiniteQuery } from "react-query";
+import { getProducts } from "@/remote/products";
+import { flatten } from "lodash";
 import InfiniteScroll from "react-infinite-scroll-component";
-import {useCallback, useState} from "react";
+import { useCallback, useState } from "react";
 import Image from "next/image";
 import Loader from "@/component/loader/Loader";
-import styled from 'styled-components';
+import styled from "styled-components";
+import { Flex } from "../flex/Flex";
 
 const List = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-wrap: wrap;
-`
-export default function ProductList(props: { mainCtgr: any, subCtgr: any }) {
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-    const {mainCtgr, subCtgr} = props;
-    const {
-        data, hasNextPage = false, fetchNextPage, isFetching
-    } = useInfiniteQuery(['products'], ({pageParam}) => {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+`;
+export default function ProductList(props: { mainCtgr: any; subCtgr: any }) {
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const { mainCtgr, subCtgr } = props;
+	const {
+		data,
+		hasNextPage = false,
+		fetchNextPage,
+		isFetching,
+	} = useInfiniteQuery(
+		["products"],
+		({ pageParam }) => {
+			return getProducts(mainCtgr, subCtgr, pageParam);
+		},
+		{
+			getNextPageParam: (snapShot) => {
+				return snapShot.lastVisible;
+			},
+		}
+	);
 
-        return getProducts(mainCtgr, subCtgr, pageParam);
-    }, {
-        getNextPageParam: (snapShot) => {
-            return snapShot.lastVisible;
-        }
-    });
+	const loadMore = useCallback(() => {
+		if (!hasNextPage || isFetching) {
+			return;
+		}
+		fetchNextPage();
+	}, [fetchNextPage, hasNextPage, isFetching]);
+	if (data == null) {
+		return null;
+	}
 
-    const loadMore = useCallback(() => {
-        if (!hasNextPage || isFetching) {
-            return
-        }
-        fetchNextPage();
-    }, [fetchNextPage, hasNextPage, isFetching])
-    if (data == null) {
-        return null;
-    }
-
-    console.log(data);
-    const products = flatten(data?.pages.map(({items}) => items));
-    return (<div style={{margin: "5px"}}>
-            <InfiniteScroll
-                dataLength={products.length}
-                hasMore={hasNextPage}
-                loader={<Loader basic={true}/>}
-                next={loadMore}
-                scrollThreshold={"100px"}
-            >
-                <List>
-                    {products.map((product, idx) => {
-                        console.log(product);
-                        return (<ProductListRow
-                            key={idx}
-                            left={<div>
-                                <p style={{
-                                    overflow: "hidden",
-                                    textOverflow: "ellipsis",
-                                    whiteSpace: "nowrap",
-                                    width: "170px",
-                                }}>
-                                    {product.name}
-                                </p>
-
-
-                            </div>}
-                            contents={<ProductRow.Text title={product.price.toString()}/>}
-                            right={ <Image
-                                src={product.imageUrl[0]}
-                                width={100}
-                                height={150}
-                                alt="상품이미지"
-                            />}
-                            // onClick={}
-                        />)
-                    })}
-                </List>
-            </InfiniteScroll>
-        </div>)
+	console.log(data);
+	const products = flatten(data?.pages.map(({ items }) => items));
+	return (
+		<div>
+			<InfiniteScroll
+				dataLength={products.length}
+				hasMore={hasNextPage}
+				loader={<Loader basic={true} />}
+				next={loadMore}
+				scrollThreshold={"100px"}
+			>
+				<List>
+					{products.map((product, idx) => {
+						console.log(product);
+						return (
+							<ProductListRow
+								key={idx}
+								brand={
+									<div
+										style={{
+											fontSize: "12px",
+											marginBottom: "10px",
+										}}
+									>
+										{product.brand}
+									</div>
+								}
+								name={
+									<div>
+										<p
+											style={{
+												fontSize: "15px",
+												lineHeight: "1.25",
+												fontWeight: "bold",
+												marginBottom: "8px",
+											}}
+										>
+											{product.name}
+										</p>
+									</div>
+								}
+								price={
+									<>
+										<Flex
+											direction="row"
+											justify="space-between"
+											style={{
+												marginBottom: "3px",
+											}}
+										>
+											<div
+												style={{
+													fontSize: "17px",
+													fontWeight: "bold",
+												}}
+											>
+												{(
+													Math.floor(
+														(product.price *
+															(100 -
+																product.discount)) /
+															100 /
+															10
+													) * 10
+												).toLocaleString() + "원"}
+											</div>
+											{product.discount ? (
+												<div
+													style={{
+														fontSize: "17px",
+														fontWeight: "bold",
+														color: "#ff2626",
+													}}
+												>
+													{product.discount + "%"}
+												</div>
+											) : null}
+										</Flex>
+										<div
+											style={{
+												fontSize: "12px",
+                                                textDecoration: "line-through",
+                                                color: "#888",
+											}}
+										>
+											{product.price + "원"}
+										</div>
+									</>
+								}
+								image={
+									<Image
+										style={{
+											height: "230px",
+											width: "200px",
+										}}
+										src={product.imageUrl[0]}
+										width={100}
+										height={150}
+										alt="상품이미지"
+									/>
+								}
+								// onClick={}
+							/>
+						);
+					})}
+				</List>
+			</InfiniteScroll>
+		</div>
+	);
 }
--- src/component/products/ProductRow.tsx
+++ src/component/products/ProductRow.tsx
@@ -1,46 +1,49 @@
 import React from "react";
+import { Flex } from "../flex/Flex";
 
 interface IProductProps {
-    left?: React.ReactNode;
-    contents: React.ReactNode;
-    right?: React.ReactNode;
-    onClick?: () => void;
+	name?: React.ReactNode;
+	price: React.ReactNode;
+	discountedPrice?: React.ReactNode;
+	discount?: React.ReactNode;
+	image?: React.ReactNode;
+	brand?: React.ReactNode;
+	onClick?: () => void;
 }
-function ListRowText({title}: {title: string}) {
-    return (
-        <div style={{fontSize: '17px'}}>
-            {title}
-        </div>
-    )
+function ListRowText({ title }: { title: string }) {
+	return <div style={{ fontSize: "20px" }}>{title}</div>;
 }
 function ProductListRow(props: IProductProps) {
-    const {left, right, contents, onClick} = props;
-    return (
-        <div style={{
-            width: "48%",
-            padding: "5px",
-            margin: "5px 3px",
-            border: "1px solid gray",
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            // height: "200px"
-        }}>
-            <div>
-                {left}
-            </div>
-
-            <div>
-                {right}
-            </div>
-            <div>
-                {contents}
-            </div>
-        </div>
-    )
+	const { name, image, price, brand, onClick } = props;
+	return (
+		<div
+			style={{
+				width: "50%",
+				// padding: "5px",
+				// margin: "5px 3px",
+				// border: "1px solid gray",
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "start",
+				// height: "200px"
+			}}
+		>
+			<div>{image}</div>
+			<div style={{margin: "15px 4px", height: '100px'}}>
+				<div>{brand}</div>
+				<div>{name}</div>
+				<div>{price}</div>
+			</div>
+			{/* <Flex direction="row">
+				<div>{price}</div>
+				<div>{discountedPrice}</div>
+				{discount ? <div>{discount}</div> : null}
+			</Flex> */}
+		</div>
+	);
 }
 
-ProductListRow.Text = ListRowText
+ProductListRow.Text = ListRowText;
 
 export default ProductListRow;
