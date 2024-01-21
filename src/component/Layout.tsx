bce2cb90be58a95af6f5d162ed09c47e8d68cd98
chaeyoo <chaeh0124@gmail.com>
2024-01-21 21:50:52 +0900

    [FEAT] scroll footer 숨기기

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -7,19 +7,43 @@ import { GiBackwardTime } from "react-icons/gi";
 import { RiHome3Line } from "react-icons/ri";
 import { SlBasket } from "react-icons/sl";
 import useUser from "@/hooks/auth/useUser";
+import { useScrollDirection } from "@/hooks/useWindow";
+import styled from "styled-components";
 
 interface LayoutProps {
 	children: ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps) {
+const Footer = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	position: fixed;
+	width: 100%;
+	bottom: 0rem;
+	height: 50px;
+	background: black;
+`;
 
+const FooterDown = styled.div`
+	display: flex;
+	justify-content: space-evenly;
+	align-items: center;
+	position: fixed;
+	width: 100%;
+	bottom: -3rem;
+	height: 50px;
+	background: black;
+`;
+
+export default function Layout({ children }: LayoutProps) {
+	const scrollDirection = useScrollDirection();
 	return (
 		<div className="layout">
 			<div className="navbar">
 				<div className="navbar__top">
 					<div className="navbar__logo">CSHOP</div>
-					
+
 					<div className="navbar__icons">
 						<SlBasket size="19" />
 					</div>
@@ -41,23 +65,39 @@ export default function Layout({ children }: LayoutProps) {
 				</div>
 			</div>
 			<div className="main">{children}</div>
-			<div className="footer">
-				<Link href="/categories">
-					<BiMenu size={22} color={"white"} />
-				</Link>
-				<Link href="/like">
-					<CiHeart size={22} color={"white"} />
-				</Link>
-				<Link href="/">
-					<RiHome3Line size={23} color={"white"} />
-				</Link>
-				<Link href="/mypage">
-					<IoPersonOutline size={22} color={"white"} />
-				</Link>
-				<Link href="/recent">
-					<GiBackwardTime size={22} color={"white"} />
-				</Link>
-			</div>
+
+			{scrollDirection === "down" && (
+				<FooterDown>
+					<FooterList />
+				</FooterDown>
+			)}
+			{scrollDirection !== "down" && (
+				<Footer>
+					<FooterList />
+				</Footer>
+			)}
 		</div>
+	);
+}
+
+function FooterList() {
+	return (
+		<>
+			<Link href="/categories">
+				<BiMenu size={22} color={"white"} />
+			</Link>
+			<Link href="/like">
+				<CiHeart size={22} color={"white"} />
+			</Link>
+			<Link href="/">
+				<RiHome3Line size={23} color={"white"} />
+			</Link>
+			<Link href="/mypage">
+				<IoPersonOutline size={22} color={"white"} />
+			</Link>
+			<Link href="/recent">
+				<GiBackwardTime size={22} color={"white"} />
+			</Link>
+		</>
 	);
 }
