be919c50ac7247f839c1424b794acafb94900c09
chaeyoo <chaeh0124@gmail.com>
2024-02-05 22:42:06 +0900

    [FIX] 에러수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,8 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en">
-			<Head>
-			<style>
-                    @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@500&display=swap");
-                </style>
-			</Head>
+			<Head/>
+			
 			<body>
 				<Main />
 				<div id="root-portal" />
